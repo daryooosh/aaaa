4810a2adbccc42f2dfd4bffffaac1a04079cb234
Ali Tavakoli <eng.tavakoli110@gmail.com>
2019-12-08 21:49:20 +0330

    Init commit.
    Version : not specified.
    State :
    Status :
    
    Only a empty project is created.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=doorPhoneFingerprintReceiver.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/doorPhoneFingerprintReceiver.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=doorphonefingerprintreceiver.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/doorphonefingerprintreceiver.x.tar
